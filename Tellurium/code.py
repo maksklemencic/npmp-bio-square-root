ee4e7d1c90d68509cf881df4cbcb3f61d24c4be9
Maks Klemenčič <maks.klemencic6@gmail.com>
2026-01-05 18:29:07 +0100

    adding better tellurium visualization

--- Tellurium/code.py
+++ Tellurium/code.py
@@ -1,4 +1,6 @@
-import tellurium as te 
+import tellurium as te
+import matplotlib.pyplot as plt
+import math
 
 def main():
     while True:
@@ -18,14 +20,14 @@ def main():
         X = {X_val}
         Y = 0
 
-        k1 = 1
-        k2 = 0.5
+        k1 = 0.01
+        k2 = 0.005
     """
 
     model = model_template.format(X_val=x_input)
 
     rr = te.loadAntimonyModel(model)
-    rr.simulate(0, 3, 200)
+    result =rr.simulate(0, 200, 1000)
 
     ss = rr.getSteadyStateValues()
     steady_x, steady_y = ss[0], ss[1]
@@ -33,7 +35,17 @@ def main():
     print(f"\nInput X value: {steady_x:.5f}")
     print(f"Approximated sqrt(X): {steady_y:.5f}")
 
-    rr.plot()
+    plt.plot(result[:, 0], result[:, 2], linewidth=1.5, label='Y')
+
+    actual_sqrt = math.sqrt(x_input)
+    plt.axhline(y=actual_sqrt, color='red', linestyle='--', linewidth=1.5, alpha=0.7)
+    plt.text(result[-1, 0] * 0.05, actual_sqrt, f'{actual_sqrt:.5f}',
+             color='red', va='bottom')
+
+    plt.xlabel("Čas")
+    plt.ylabel("Koncentracija")
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     main()
